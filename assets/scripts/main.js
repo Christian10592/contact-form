0d96d7d0642e6540ce9dd24101045b5afc76b317
Christian Smitherman <hello@christiansmitherman.com>
2016-05-11 21:12:55 -0500

    completed main menu and mobile menu... started header section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,163 +1,25 @@
 $(document).ready(function(){
+//==================== Selectors ===============//
 
-//==================== TODO ==================== //
-//Clean Up and Organize
-//Infinite Slider Loop
-//Better Documentation
-//Change main classes to IDs
-//Change SCSS to BEM
+var mainMenuSelector = $('nav.main .menu');
+var mainMenuOverlay  = $('nav.main .right');
 
-//==================== List of Selectors ====================//
+//==================== Globals ================//
 
-//Slider
-var sliderContainer   = $('.slider-container');
-var slides            = $('.slides');
-var slide             = $('.slide');
-var firstSlide        = $('.slide:first-child');
-var lastSlide         = $('.slide:last-child');
+//==================== Nav ====================//
+var mobileMenuOpen = false;
 
-//Controls
-var controlsIcon      = $('.controls i');
-var controlsContainer = $('.controls');
-
-//Bullets
-var bullets           = $('.bullets');
-var bullet            = $('.bullet');
-var activeBullet      = $('.active-bullet');
-var firstBullet       = $('.bullet:nth-child(1)');
-
-//==================== Global Variables ======================//
-//actionSlide
-var actionSlide;
-
-//Set Slider Position
-var slidePos = 1;
-
-//Slide Speed
-var slideSpeed = 300;
-
-//Starting bullet position
-var bulletNav = 1;
-
-//Controls hidden on start
-var controlsVisible = false;
-
-//==================== Calculate Sizes ======================//
-
-//Count Slides
-var countSlides = slide.length;
-
-//Find Slide Width
-var slideWidth = parseInt(slide.css('width'));
-
-//Set Slider Container Width (add 2 to account for cloned slides for infinite loop)
-var sliderWidth = slideWidth * (countSlides + 2) + 'px';
-
-//Get height of the controls
-var controlHeight = parseInt($('.controls').css('height'));
-
-//Get height of slider
-var sliderHeight = parseInt($('.slider-container').css('height'));
-
-//Center the controls -- subtract the height of the slider by the height of the controls divide by 2 and add px to the end
-var centerControls = (sliderHeight - controlHeight) / 2 + 'px';
-
-//==================== Build Bullets =======================//
-
-//For each slide create a bullet
-for (i = 0; i < countSlides; i++) {
-    bullets.append('<li class="bullet"></li>');
-}
-
-//Set first bullet active by default
-$('.bullet:nth-child(1)').addClass('active-bullet');
-
-//Single Bullet Width
-var bulletWidth = parseInt($('.bullet').css('width')) * countSlides;
-
-//Get Bullet Padding
-var bulletPadding = parseInt($('.bullet').css('margin-right'));
-
-//Multiply bullet padding by the amount of slides and subtract last bullet's padding
-var bulletTotalPadding = bulletPadding * countSlides - bulletPadding;
-
-//Add the width of a single bullet to the total padding of all bullets and add px to the end
-var calBulletContainer = bulletWidth + bulletTotalPadding + 'px';
-
-//Set Bullet Container Width
-bullets.css({'width': calBulletContainer});
-
-//Change Active Bullet
-function changeActiveBullet(current) {
-  $('.active-bullet').removeClass('active-bullet');
-  $('.bullet:nth-child(' + current + ')').addClass('active-bullet');
-}
-
-//==================== Slide Animations =====================//
-
-//Generate Slider Width
-//Offset first slide to account for cloned slide
-slides.css({
-  'width': sliderWidth,
-  'margin-left': '-' + slideWidth + 'px'
-});
-
-//Centers the controls
-controlsContainer.css({'margin-top': centerControls});
-
-//Function determines which direction was clicked by passing a param that represents the class of the button clicked also changes the active bullet position by 1
-function actionSlide(direction) {
-  if (direction == 'prev') {
-    doSlide = '+=' + slideWidth + 'px';
-    bulletNav--;
-    if (slidePos == 0) {
-      slides.css({'margin-left': ('-' + countSlides * slideWidth) + 'px'});
-      bulletNav = countSlides;
-      slidePos = countSlides;
-    }
+$('#menu-icon').click(function(){
+  if (mobileMenuOpen) {
+    mainMenuSelector.animate({'right': '300px'});
+    mainMenuOverlay.fadeOut();
+    mobileMenuOpen = false;
   } else {
-    doSlide = '-=' + slideWidth + 'px';
-    bulletNav++;
-    if (slidePos > countSlides) {
-     slide.css({'margin-left': '-' + slideWidth + 'px'});
-     bulletNav = 1;
-     slidePos = 1;
-   }
+    mainMenuOverlay.fadeIn();
+    mainMenuSelector.animate({'right': '0px'});
+    mobileMenuOpen = true;
   }
-  changeActiveBullet(bulletNav)
-  slides.animate({'margin-left': doSlide}, slideSpeed);
-}
 
-//When a control is clicked gets controls class name and uses that as the param for actionSlide()
-controlsIcon.click(function(){
-  var slideDirection = $(this).parent().attr('class');
-  actionSlide(slideDirection);
 });
-
-sliderContainer.hover(function(){
-  if (controlsVisible) {
-    controlsContainer.fadeOut();
-    bullets.fadeOut();
-    controlsVisible = false;
-  } else {
-    controlsContainer.fadeIn();
-    bullets.fadeIn();
-    controlsVisible = true;
-  }
-});
-
-//==================== Infinite Loop ========================//
-
-//Clone First Slide
-firstSlide.before(lastSlide.clone(true));
-//Clone Second Slide
-lastSlide.after(firstSlide.clone(true));
-
-
-
-
-
-
-
 //End Document.ready
 });
