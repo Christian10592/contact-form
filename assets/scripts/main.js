ac616088141d471ddd3d6a94115eecdd5e248601
Christian Smitherman <christian@propellerideas.com>
2016-05-13 11:06:33 -0400

    basic style and structure of contact form

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,25 +1,9 @@
 $(document).ready(function(){
 //==================== Selectors ===============//
 
-var mainMenuSelector = $('nav.main .menu');
-var mainMenuOverlay  = $('nav.main .right');
-
 //==================== Globals ================//
 
-//==================== Nav ====================//
-var mobileMenuOpen = false;
+//==================== Contact Form ===========//
 
-$('#menu-icon').click(function(){
-  if (mobileMenuOpen) {
-    mainMenuSelector.animate({'right': '300px'});
-    mainMenuOverlay.fadeOut();
-    mobileMenuOpen = false;
-  } else {
-    mainMenuOverlay.fadeIn();
-    mainMenuSelector.animate({'right': '0px'});
-    mobileMenuOpen = true;
-  }
-
-});
 //End Document.ready
 });
